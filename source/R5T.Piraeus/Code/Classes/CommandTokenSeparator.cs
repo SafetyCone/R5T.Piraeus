0db319f1580133437cad1eccf7d105e0f0890780
MinexAutomation <minex.automation@gmail.com>
2019-12-12 22:41:29 -0800

    Added CommandTokenSeprator.DefaultValue.

---
+++ source/R5T.Piraeus/Code/Classes/CommandTokenSeparator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Magyar;
+
+
+namespace R5T.Piraeus
+{
+    public static class CommandTokenSeparator
+    {
+        public const string DefaultValue = StringHelper.Space;
+    }
+}
