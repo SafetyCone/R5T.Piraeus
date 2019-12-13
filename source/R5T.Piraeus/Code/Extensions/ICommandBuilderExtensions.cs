ae02e33d7d4a2af7f4cb4ca018c7383ddf995a73
MinexAutomation <minex.automation@gmail.com>
2019-12-12 23:02:21 -0800

    Added ICommandBuilder extension to properly quote a path token.

---
+++ source/R5T.Piraeus/Code/Extensions/ICommandBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace R5T.Piraeus
+{
+    public static class ICommandBuilderExtensions
+    {
+        /// <summary>
+        /// Properly quotes a path token.
+        /// Surrounds the provided path with quotes in case of spaces in the path.
+        /// </summary>
+        public static void AppendPath(this ICommandBuilder commandBuilder, string path)
+        {
+            var appendix = $"\"{path}\"";
+
+            commandBuilder.Append(appendix);
+        }
+    }
+}
